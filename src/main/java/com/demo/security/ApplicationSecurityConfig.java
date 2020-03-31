3acfa67d1e7a3be62a17b449c9efbb9bc2306784
vladtf <vendeta0263@gmail.com>
2020-03-31 20:38:25 +0300

    Add index,html page

--- src/main/java/com/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/demo/security/ApplicationSecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-public class ApplicationSecuirtyConfig extends WebSecurityConfigurerAdapter {
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
