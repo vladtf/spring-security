1bbb101ed79c99ccebd119d97ecf1bd8f94d2ed8
vladtf <vendeta0263@gmail.com>
2020-04-03 20:45:16 +0300

    Changed default url on success login

--- src/main/java/com/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/demo/security/ApplicationSecurityConfig.java
@@ -44,7 +44,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login").permitAll();
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/courses", true);
 //                .httpBasic();
     }
 
