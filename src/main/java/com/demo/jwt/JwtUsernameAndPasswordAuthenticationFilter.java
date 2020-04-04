e1bfbb8e2f0c11f2b6285835b9bc60725260d2dc
vladtf <vendeta0263@gmail.com>
2020-04-04 23:10:31 +0300

    Fixed expiration day configuration

--- src/main/java/com/demo/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/demo/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -61,7 +61,7 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
                 .setSubject(authResult.getName())
                 .claim("authorities", authResult.getAuthorities())
                 .setIssuedAt(new Date())
-                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2)))
+                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusDays(jwtConfig.getTokenExpirationAfterDays())))
                 .signWith(secretKey)
                 .compact();
 
