e0c30f68d081c2e8129668d1600df9b1f368a61f
vladtf <vendeta0263@gmail.com>
2020-04-04 15:11:41 +0300

    Implemented dao as fake in memory database FakeApplicationUserDaoService

--- src/main/java/com/demo/auth/ApplicationUser.java
+++ src/main/java/com/demo/auth/ApplicationUser.java
@@ -4,28 +4,28 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
+import java.util.Set;
 
 public class ApplicationUser implements UserDetails {
 
-    private final List<? extends GrantedAuthority> grantedAuthorities;
     private final String password;
     private final String username;
+    private final Set<? extends GrantedAuthority> grantedAuthorities;
     private final boolean isAccountNonExpired;
     private final boolean isAccountNonLocked;
     private final boolean isCredentialsNonExpired;
     private final boolean isEnabled;
 
-    public ApplicationUser(List<? extends GrantedAuthority> grantedAuthorities,
-                           String password,
+    public ApplicationUser(String password,
                            String username,
+                           Set<? extends GrantedAuthority> grantedAuthorities,
                            boolean isAccountNonExpired,
                            boolean isAccountNonLocked,
                            boolean isCredentialsNonExpired,
                            boolean isEnabled) {
-        this.grantedAuthorities = grantedAuthorities;
         this.password = password;
         this.username = username;
+        this.grantedAuthorities = grantedAuthorities;
         this.isAccountNonExpired = isAccountNonExpired;
         this.isAccountNonLocked = isAccountNonLocked;
         this.isCredentialsNonExpired = isCredentialsNonExpired;
